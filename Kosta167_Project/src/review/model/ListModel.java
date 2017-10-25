d61f6a401f3866865dbfd5f8781b1d6e594e7375
kosta <kosta@kosta-PC>
2017-10-25 17:38:48 +0900

    승세야 행복하자

---
+++ Kosta167_Project/src/review/model/ListModel.java
@@ -0,0 +1,56 @@
+package review.model;
+
+import java.util.List;
+
+public class ListModel {
+	private List<Review> list;
+	private int requestPage;
+	private int totalPageCount;
+	private int startPage;
+	private int endPage;
+	
+	public ListModel() {	}
+	
+	public ListModel(List<Review> list, int requestPage, int totalPageCount, int startPage, int endPage) {
+		this.list = list;
+		this.requestPage = requestPage;
+		this.totalPageCount = totalPageCount;
+		this.startPage = startPage;
+		this.endPage = endPage;
+	}
+
+
+
+	public List<Review> getList() {
+		return list;
+	}
+	public void setList(List<Review> list) {
+		this.list = list;
+	}
+	public int getRequestPage() {
+		return requestPage;
+	}
+	public void setRequestPage(int requestPage) {
+		this.requestPage = requestPage;
+	}
+	public int getTotalPageCount() {
+		return totalPageCount;
+	}
+	public void setTotalPageCount(int totalPageCount) {
+		this.totalPageCount = totalPageCount;
+	}
+	public int getStartPage() {
+		return startPage;
+	}
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+	public int getEndPage() {
+		return endPage;
+	}
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+	
+	
+}
